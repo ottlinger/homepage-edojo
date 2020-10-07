d12df8d9339c003141137e52291588cca03ab6d2
Hugo Hirsch <github@hugo-hirsch.de>
2020-10-07 14:23:22 +0200

    Remove GA

--- index.php
+++ index.php
@@ -22,19 +22,6 @@ echo '<?xml version="1.0" encoding="utf-8"?>';
 <title>eDojo.org - easy dojo management</title>
 </head>
 <body>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-12555678-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
-  })();
-
-</script>
 					<h1>eDojo.org - easy dojo management</h1>
 			<table class="index">
 				<tr class="mittig">
