2d8260874b2f56b454af2242a0bd3666bfba8d53
Hugo Hirsch <github@hugo-hirsch.de>
2020-10-07 14:27:47 +0200

    Updates

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$dateOfChange = '2018-05-25';
+$dateOfChange = '2020-10-07';
 echo '<?xml version="1.0" encoding="utf-8"?>';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
