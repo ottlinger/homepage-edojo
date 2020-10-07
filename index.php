07625cd485d47e86966fbdec1657b4d71c886edd
Hugo Hirsch <github@hugo-hirsch.de>
2020-10-07 13:35:22 +0200

    Project init

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+$dateOfChange = '2018-05-25';
+echo '<?xml version="1.0" encoding="utf-8"?>';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<link rel="stylesheet" href="./css/styleTux.css" type="text/css" />
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+<meta name="DC.Title" content="eDojo.org - easy dojo management"/>
+<meta name="DC.Author" content="Aiki IT Berlin"/>
+<meta name="DC.Publisher" content="Aiki IT Berlin"/>
+<meta name="DC.Copyright" content="(C) Aiki IT Berlin 1996-<?php echo date('Y'); ?>"/>
+<meta name="Revisit" content="After 5 days"/>
+<meta name="DC.keywords" content="a software for managing dojos electronically"/>
+<meta name="DC.Description" content="Praesentationsseiten"/>
+<meta name="Robots" content="INDEX,FOLLOW"/>
+<meta name="DC.Language" content="de"/>
+<meta name="DC.Date" content="<?php echo $dateOfChange; ?>"/>
+<title>eDojo.org - easy dojo management</title>
+</head>
+<body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12555678-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+  })();
+
+</script>
+					<h1>eDojo.org - easy dojo management</h1>
+			<table class="index">
+				<tr class="mittig">
+					<td class="mittig">
+						eDojo is hosted at Github under the following licence:
+						<ul>
+								<li>source code:<a onclick="window.open(this.href,'_blank');return false;" href="https://www.gnu.org/licenses/gpl.html">GPL v3</a></li>
+								<li>additional content:<a onclick="window.open(this.href,'_blank');return false;" href="https://creativecommons.org/licenses/by/3.0/">CreativeCommons 3 BY</a></li>
+								<li>project home at <a onclick="window.open(this.href,'_blank');return false;" href="https://github.com/ottlinger/edojo/">Github edojo</a></li>
+						</ul>
+					</td>
+				</tr>
+				<tr>
+					<td class="mittig">
+						If eDojo is completed, it's supposed to help you with the following things:
+						<ul>
+								<li>manage your dojo's members electronically</li>
+								<li>manage member fees and payment information</li>
+								<li>manage graduation fees/ status</li>
+								<li>manage and book payment of trainers</li>
+								<li>keep track of member attendance</li>
+								<li>visualize your dojo's success</li>
+                <li>available subproject to manage seminar subscriptions: <a onclick="window.open(this.href,'_blank');return false;" href="https://github.com/ottlinger/hornherzogen">Herzogenhorn</a></li>
+						</ul>
+														<br /><br />
+							If you need help or would like to participate in the development - feel free to contact us
+
+<a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#99;&#111;&#110;&#116;&#97;&#99;&#116;&#64;&#101;&#100;&#111;&#106;&#111;&#46;&#111;&#114;&#103;&#63;&#115;&#117;&#98;&#106;&#101;&#99;&#116;&#61;&#119;&#101;&#98;&#112;&#97;&#103;&#101;" title="Kontakt- und Infomail">
+&#99;&#111;&#110;&#116;&#97;&#99;&#116;&#64;&#101;&#100;&#111;&#106;&#111;&#46;&#111;&#114;&#103;
+</a>
+														<br />
+														<br />
+                                        </td>
+				</tr>
+				<tr class="mittig">
+					<td class="mittig">
+						eDojo's technical requirements are:
+						<ul>
+								<li>relational database such as <a onclick="window.open(this.href,'_blank');return false;" href="https://www.postgresql.org/">PostgreSQL</a> or
+								<a onclick="window.open(this.href,'_blank');return false;" href="https://www.mysql.de/">MySQL</a> or</li>
+								<li>Java <a onclick="window.open(this.href,'_blank');return false;" href="https://java.sun.com/">Java Development Kit (JDK)-Download</a></li>
+								<li>Spring / Spring Boot <a onclick="window.open(this.href,'_blank');return false;" href="https://projects.spring.io/spring-boot/">Spring Boot project webpage</a></li>
+						</ul>
+					</td>
+				</tr>
+			</table>
+	<address>
+	last update: <?php echo $dateOfChange; ?> | &copy; eDojo.org-team
+	<br />
+					<a onclick="window.open(this.href,'_blank');return false;" href="https://validator.w3.org/check/referer">
+					<img src="pic/valid-xhtml11" alt="Valid XHTML 1.1!" style="width:44px; height:16px; border:0;"/></a>
+                    <a onclick="window.open(this.href,'_blank');return false;" href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fedojo.org%2Fcss%2FstyleTux.css&amp;profile=css3svg&amp;usermedium=all&amp;warning=1&amp;vextwarning=">
+		    		<img src="pic/vcss" alt="Valid CSS!" style="width:44px; height:16px; border:0;" /></a>
+				<a rel="license" href="https://creativecommons.org/licenses/by/3.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/3.0/80x15.png" /></a>
+				licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/3.0/">Creative Commons licence</a>
+	</address>
+	</body>
+</html>
