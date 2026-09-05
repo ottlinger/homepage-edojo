62be6f006631e555e9c27e027f781b69b3935e9f
Hugo Hirsch <github@hugo-hirsch.de>
2026-09-05 23:14:12 +0200

    Fix order and add more info

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ New homepage of https://edojo.org
 ![Logo](./pic/edojoTransparentSmall.png)
 
 
-# Background
+# Pictures 
 
 * https://unsplash.com/de/fotos/ein-zimmer-mit-einem-grossen-runden-fenster-und-blumen-in-einem-korb-Apozpg8oXUk
 * https://unsplash.com/de/fotos/ein-grosser-leerer-raum-D5A-TwxRpUg
+* https://unsplash.com/de/fotos/abstrakte-defokussierung-digitale-technologie-hintergrund-reprasentieren-big-data-und-digital-0EaCWqANolk
+* https://unsplash.com/de/fotos/ein-stapel-blauer-kisten-mit-roten-lichtern-darin-nUbcA9rGZSg
+* https://unsplash.com/de/fotos/photo-of-5-story-library-building-PkbZahEG2Ng
+* https://unsplash.com/de/fotos/ein-stapel-gestapelter-blauer-und-weisser-teller-n7eJHQwefeI
--- index.php
+++ index.php
@@ -71,11 +71,18 @@
 				<section id="banner">
 					<header>
 						<h2>eDojo - your electronic dojo management software</h2>
-						<p>
-							eDojo is hosted at GitHub;
+                        <p>
+                            eDojo is hosted at GitHub;
                             project home is: <a href="https://github.com/ottlinger/edojo/">edojo</a>
                             source code: <a href="https://www.gnu.org/licenses/gpl.html">GPL v3</a> licensed
-						</p>
+                        </p>
+                        <ul>
+                            <li><strong>Open source</strong> – not a proprietary black box</li>
+                            <li><strong>Self-hosted</strong> – full control over your own data</li>
+                            <li><strong>Designed for dojos</strong> – rather than generic club software</li>
+                            <li>No unnecessary complexity</li>
+                            <li>Data protection-friendly</li>
+                        </ul>
                         <p>
                             Technologies in use are:
                         </p>
