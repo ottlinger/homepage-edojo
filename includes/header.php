95e7394f003d55f9ba63142393b75da280b64906
P. Ottlinger <ottlinger@365farmnet.com>
2026-09-12 16:10:57 +0200

    Modularize header and reformat

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<div id="header">
+    <div class="inner">
+        <header>
+            <h1><a href="/" id="logo">eDojo</a></h1>
+            <hr />
+            <p>easy dojo management</p>
+        </header>
+        <footer>
+            <a href="#banner" class="button circled scrolly">Enter</a>
+        </footer>
+    </div>
+    <nav id="nav">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="#techstack">TechStack</a></li>
+            <li><a href="#edojo-features">Features</a></li>
+            <li><a href="#footer">Contact</a></li>
+            <?php
+            /*
+            <li>
+                <a href="#">Dropdown</a>
+                <ul>
+                    <li><a href="#">Lorem ipsum dolor</a></li>
+                    <li><a href="#">Magna phasellus</a></li>
+                    <li><a href="#">Etiam dolore nisl</a></li>
+                    <li>
+                        <a href="#">And a submenu &hellip;</a>
+                        <ul>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Phasellus consequat</a></li>
+                            <li><a href="#">Magna phasellus</a></li>
+                            <li><a href="#">Etiam dolore nisl</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Veroeros feugiat</a></li>
+                </ul>
+            </li>
+            */
+            ?>
+        </ul>
+    </nav>
+</div>
--- index.php
+++ index.php
@@ -1,85 +1,46 @@
 <!DOCTYPE HTML>
 <html lang="en">
-	<head>
-        <title>eDojo.org - easy dojo management</title>
-        <meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="DC.Title" content="eDojo.org - easy dojo management"/>
-        <meta name="DC.Author" content="Aiki IT Berlin, Beelitz"/>
-        <meta name="DC.Publisher" content="Aiki IT Berlin, Beelitz"/>
-        <meta name="DC.Copyright" content="(C) Aiki IT Berlin, Beelitz 1996-2026"/>
-        <meta name="Revisit" content="After 5 days"/>
-        <meta name="DC.keywords" content="a software for managing dojos electronically"/>
-        <meta name="Robots" content="INDEX,FOLLOW"/>
-        <meta name="DC.Language" content="english"/>
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
-	</head>
-	<body class="homepage is-preload">
-		<div id="page-wrapper">
-			<!-- Header -->
-				<div id="header">
-						<div class="inner">
-							<header>
-								<h1><a href="/" id="logo">eDojo</a></h1>
-								<hr />
-								<p>easy dojo management</p>
-							</header>
-							<footer>
-								<a href="#banner" class="button circled scrolly">Enter</a>
-							</footer>
-						</div>
-						<nav id="nav">
-							<ul>
-                                <li><a href="/">Home</a></li>
-                                <li><a href="#techstack">TechStack</a></li>
-                                <li><a href="#edojo-features">Features</a></li>
-                                <li><a href="#footer">Contact</a></li>
-                                <?php
-                                /*
-								<li>
-									<a href="#">Dropdown</a>
-									<ul>
-										<li><a href="#">Lorem ipsum dolor</a></li>
-										<li><a href="#">Magna phasellus</a></li>
-										<li><a href="#">Etiam dolore nisl</a></li>
-										<li>
-											<a href="#">And a submenu &hellip;</a>
-											<ul>
-												<li><a href="#">Lorem ipsum dolor</a></li>
-												<li><a href="#">Phasellus consequat</a></li>
-												<li><a href="#">Magna phasellus</a></li>
-												<li><a href="#">Etiam dolore nisl</a></li>
-											</ul>
-										</li>
-										<li><a href="#">Veroeros feugiat</a></li>
-									</ul>
-								</li>
-                                */
-                                ?>
-							</ul>
-						</nav>
-				</div>
-
-            <?php include 'includes/banner.php'; ?>
-            <?php include 'includes/carousel.php'; ?>
-            <?php include 'includes/features.php'; ?>
-            <?php include 'includes/main.php'; ?>
-            <?php include 'includes/footer.php'; ?>
-        </div>
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-	</body>
+<head>
+    <title>eDojo.org - easy dojo management</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="manifest" href="/site.webmanifest"/>
+    <meta name="DC.Title" content="eDojo.org - easy dojo management"/>
+    <meta name="DC.Author" content="Aiki IT Berlin, Beelitz"/>
+    <meta name="DC.Publisher" content="Aiki IT Berlin, Beelitz"/>
+    <?php
+    echo '<meta name="DC.Copyright" content="(C) Aiki IT Berlin, Beelitz 1996-' . date("Y") . '"/>';
+    ?>
+    <meta name="Revisit" content="After 5 days"/>
+    <meta name="DC.keywords" content="a software for managing dojos electronically"/>
+    <meta name="Robots" content="INDEX,FOLLOW"/>
+    <meta name="DC.Language" content="english"/>
+    <link rel="stylesheet" href="assets/css/main.css"/>
+    <noscript>
+        <link rel="stylesheet" href="assets/css/noscript.css"/>
+    </noscript>
+</head>
+<body class="homepage is-preload">
+<div id="page-wrapper">
+    <?php include 'includes/header.php'; ?>
+    <?php include 'includes/banner.php'; ?>
+    <?php include 'includes/carousel.php'; ?>
+    <?php include 'includes/features.php'; ?>
+    <?php include 'includes/main.php'; ?>
+    <?php include 'includes/footer.php'; ?>
+</div>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.dropotron.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/browser.min.js"></script>
+<script src="assets/js/breakpoints.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
 </html>
