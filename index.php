e757fe807750a0a7f108e50730ad8f430f773af3
P. Ottlinger <ottlinger@365farmnet.com>
2026-09-12 16:03:22 +0200

    Modularise footer

--- index.php
+++ index.php
@@ -236,34 +236,8 @@
                 </article>
             </div>
 
-            <!-- Footer -->
-				<div id="footer">
-					<div class="container">
-						<hr />
-						<div class="row">
-							<div class="col-12">
-								<!-- Contact -->
-									<section class="contact">
-										<header>
-											<h3>More ideas? Get in touch</h3>
-										</header>
-										<p>You may find the project at GitHub or contact us directly.</p>
-										<ul class="icons">
-                                            <li><a href="https://www.github.com/ottlinger/edojo" target="_blank" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
-                                            <li><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#99;&#111;&#110;&#116;&#97;&#99;&#116;&#64;&#101;&#100;&#111;&#106;&#111;&#46;&#111;&#114;&#103;&#63;&#115;&#117;&#98;&#106;&#101;&#99;&#116;&#61;&#119;&#101;&#98;&#112;&#97;&#103;&#101;" target="_blank" class="icon fa-envelope"><span class="label">E-Mail</span></a></li>
-										</ul>
-									</section>
-								<!-- Copyright -->
-									<div class="copyright">
-										<ul class="menu">
-                                            <li>&copy; 2015-<?php echo date('Y'); ?> eDojo.org team. All rights reserved.</li><li>Design: <a href="https://html5up.net/helios">Helios - HTML5 UP</a></li><li>Images taken from <a href="https://unsplash.com">unsplash.com</a></li>
-										</ul>
-									</div>
-							</div>
-						</div>
-					</div>
-				</div>
-		</div>
+            <?php include 'includes/footer.php'; ?>
+        </div>
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/jquery.dropotron.min.js"></script>
 			<script src="assets/js/jquery.scrolly.min.js"></script>
