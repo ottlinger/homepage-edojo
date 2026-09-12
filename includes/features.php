24a3a98b31543df5455fc3c853fb104ee13af917
P. Ottlinger <ottlinger@365farmnet.com>
2026-09-12 16:05:59 +0200

    Modularise main and features

---
+++ includes/features.php
@@ -0,0 +1,70 @@
+<div id="edojo-features" class="wrapper style1">
+    <section id="features" class="container special">
+        <header>
+            <h2>If eDojo is completed</h2>
+            <p>it's supposed to help you with the following things:</p>
+        </header>
+        <div class="row">
+            <article class="col-4 col-12-mobile special">
+                <a href="#edojo-features" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
+                <header>
+                    <h3><a href="#">manage your dojo's members electronically</a></h3>
+                </header>
+                <p>
+                    Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+                    porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+                </p>
+            </article>
+            <article class="col-4 col-12-mobile special">
+                <a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
+                <header>
+                    <h3><a href="#">manage member fees and payment information</a></h3>
+                </header>
+                <p>
+                    Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+                    porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+                </p>
+            </article>
+            <article class="col-4 col-12-mobile special">
+                <a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
+                <header>
+                    <h3><a href="#">manage graduation fees and status</a></h3>
+                </header>
+                <p>
+                    Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+                    porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+                </p>
+            </article>
+            <article class="col-4 col-12-mobile special">
+                <a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
+                <header>
+                    <h3><a href="#">manage and book payment of trainers</a></h3>
+                </header>
+                <p>
+                    Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+                    porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+                </p>
+            </article>
+            <article class="col-4 col-12-mobile special">
+                <a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
+                <header>
+                    <h3><a href="#">keep track of member attendance</a></h3>
+                </header>
+                <p>
+                    Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+                    porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+                </p>
+            </article>
+            <article class="col-4 col-12-mobile special">
+                <a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
+                <header>
+                    <h3><a href="#">visualise your dojo's success</a></h3>
+                </header>
+                <p>
+                    Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+                    porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+                </p>
+            </article>
+        </div>
+    </section>
+</div>
