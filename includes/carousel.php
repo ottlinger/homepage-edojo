26ad0b50c3430b60f5591c23ef24dd4570a23fbe
P. Ottlinger <ottlinger@365farmnet.com>
2026-09-12 16:06:53 +0200

    Modularise carousel

---
+++ includes/carousel.php
@@ -0,0 +1,43 @@
+<section id="techstack" class="carousel">
+    <div class="reel">
+        <article>
+            <a href="https://gradle.org/" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+            <header>
+                <h3><a href="https://gradle.org/">Gradle</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+        </article>
+
+        <article>
+            <a href="https://java.sun.com/" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+            <header>
+                <h3><a href="https://java.sun.com/">Java</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+        </article>
+
+        <article>
+            <a href="https://www.postgresql.org/" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+            <header>
+                <h3><a href="https://www.postgresql.org/">PostgreSQL</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+        </article>
+
+        <article>
+            <a href="https://projects.spring.io/spring-boot/" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+            <header>
+                <h3><a href="https://projects.spring.io/spring-boot/">Spring Boot</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+        </article>
+
+        <article>
+            <a href="https://projects.spring.io/spring-data/" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+            <header>
+                <h3><a href="https://projects.spring.io/spring-data/">Spring Data</a></h3>
+            </header>
+            <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+        </article>
+    </div>
+</section>
