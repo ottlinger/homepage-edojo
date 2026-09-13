6d617c59a8092f9fba89529bdc4c57254876f3b9
Hugo Hirsch <github@hugo-hirsch.de>
2026-09-13 22:29:37 +0200

    Rm pic links

--- includes/features.php
+++ includes/features.php
@@ -6,7 +6,7 @@
         </header>
         <div class="row">
             <article class="col-4 col-12-mobile special">
-                <a href="#edojo-features" class="image featured"><img src="images/pic07.jpg" alt=""/></a>
+                <img src="images/pic07.jpg" class="image featured" alt=""/>
                 <header>
                     <h3>manage your dojo's members electronically</h3>
                 </header>
@@ -17,7 +17,7 @@
                 </p>
             </article>
             <article class="col-4 col-12-mobile special">
-                <a href="#" class="image featured"><img src="images/pic08.jpg" alt=""/></a>
+                <img src="images/pic08.jpg" class="image featured" alt=""/>
                 <header>
                     <h3>manage member fees and payment information</h3>
                 </header>
@@ -28,9 +28,9 @@
                 </p>
             </article>
             <article class="col-4 col-12-mobile special">
-                <a href="#" class="image featured"><img src="images/pic09.jpg" alt=""/></a>
+                <img src="images/pic09.jpg" class="image featured" alt=""/>
                 <header>
-                    <h3>>manage graduation fees and status</h3>
+                    <h3>manage graduation fees and status</h3>
                 </header>
                 <p>
                     Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
@@ -39,7 +39,7 @@
                 </p>
             </article>
             <article class="col-4 col-12-mobile special">
-                <a href="#" class="image featured"><img src="images/pic07.jpg" alt=""/></a>
+                <img src="images/pic07.jpg" class="image featured" alt=""/>
                 <header>
                     <h3>manage and book payment of trainers</h3>
                 </header>
@@ -50,7 +50,7 @@
                 </p>
             </article>
             <article class="col-4 col-12-mobile special">
-                <a href="#" class="image featured"><img src="images/pic08.jpg" alt=""/></a>
+                <img src="images/pic08.jpg" class="image featured" alt=""/>
                 <header>
                     <h3>keep track of member attendance</h3>
                 </header>
@@ -61,7 +61,7 @@
                 </p>
             </article>
             <article class="col-4 col-12-mobile special">
-                <a href="#" class="image featured"><img src="images/pic09.jpg" alt=""/></a>
+                <img src="images/pic09.jpg" class="image featured" alt=""/>
                 <header>
                     <h3>visualise your dojo's success</h3>
                 </header>
